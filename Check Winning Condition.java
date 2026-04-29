91005e073a7eb0be81a34d2a62899585c234626e
Razeen <razeenh728@gmail.com>
2026-04-29 12:11:00 +0530

    UC9: Implement winning condition check (rows, columns, diagonals)

---
+++ Check Winning Condition.java
@@ -0,0 +1,44 @@
+/**
+ * UC9 - Check Winning Condition
+ */
+static boolean checkWin(char symbol) {
+
+    // Check rows
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == symbol &&
+            board[i][1] == symbol &&
+            board[i][2] == symbol) {
+            return true;
+        }
+    }
+
+    // Check columns
+    for (int j = 0; j < 3; j++) {
+        if (board[0][j] == symbol &&
+            board[1][j] == symbol &&
+            board[2][j] == symbol) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if (board[0][0] == symbol &&
+        board[1][1] == symbol &&
+        board[2][2] == symbol) {
+        return true;
+    }
+
+    if (board[0][2] == symbol &&
+        board[1][1] == symbol &&
+        board[2][0] == symbol) {
+        return true;
+    }
+
+    return false;
+}
+placeMove(row, col, playerSymbol);
+
+if (checkWin(playerSymbol)) {
+    System.out.println("Player wins!");
+    break;
+}
